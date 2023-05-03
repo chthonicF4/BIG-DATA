ca23b71cdd2db3c5396b7931318782a9a2ec57c4
chthonicF4 <107403964+chthonicF4@users.noreply.github.com>
2023-05-03 12:34:33 +0100

    plots

--- book 5.py
+++ book 5.py
@@ -73,22 +73,6 @@ seccondLepton = TLorentzVector()
 thirdLepton = TLorentzVector()
 fourthLepton = TLorentzVector()
 
-histagram = Hist(hist.axis.Regular(30,40,140, label = "Mass (GeV)"))
-# loading feedback
-prev_ldbr = -1
-total_events = len(sel_events)
-length_ldbr = 50
-
-def eta(prev:float,current:float,time:float):
-    if time == 0 : time = 0.00001
-    completedPerSec = (current - prev)/time
-    ammount_left = 1 - current
-    time_left = ammount_left / completedPerSec
-    return time_left
-
-start = time.time()
-
-
 def analise_event(event):
 
     lep_n      = event[  "lep_n"   ]
@@ -126,39 +110,85 @@ def analise_event(event):
         elif not(lep_type[combo[0]]==lep_type[combo[1]]): # same type
             continue
         else:
-            firstLepton.SetPtEtaPhiE(lep_pt[0]/1000., lep_eta[0], lep_phi[0], lep_E[0]/1000.)
-            seccondLepton.SetPtEtaPhiE(lep_pt[1]/1000., lep_eta[1], lep_phi[1], lep_E[1]/1000.)
+            firstLepton.SetPtEtaPhiE(lep_pt[combo[0]]/1000., lep_eta[combo[0]], lep_phi[combo[0]], lep_E[combo[0]]/1000.)
+            seccondLepton.SetPtEtaPhiE(lep_pt[combo[1]]/1000., lep_eta[combo[1]], lep_phi[combo[1]], lep_E[combo[1]]/1000.)
             Z0_boson_1 = firstLepton + seccondLepton
             
-            if (80<Z0_boson_1.M()<100):
-                out_bosons.append(Z0_boson_1)
+            if True or (90<Z0_boson_1.M()<92):
+                out_bosons.append((lep_type[combo[0]],Z0_boson_1))
                 blacklist.append(combo[0])
                 blacklist.append(combo[1])
-
     return out_bosons
 
 
 pass
 
+
+# loading feedback
+global prev_loading , current_loading , start ,average_list
+start = time.time()
+prev_loading = 0
+current_loading = 0
+average_list = []
+
+def eta(prev:float,current:float,time:float):
+    if time == 0 : time = 0.0001
+    completedPerSec = (current - prev)/time
+    if completedPerSec == 0 :
+        return 0
+    ammount_left = 1 - current
+    time_left = ammount_left / completedPerSec
+    return time_left
+
 def loading(value):
-    print(f"{100*(value):.2f}%\r",end="")
+    global prev_loading , current_loading , start,average_list
+    prev_loading = current_loading
+    current_loading = value
+    time_left = eta(prev_loading,current_loading,time.time()-start)
+    average_list.append(time_left)
+    if len(average_list) > 50 :
+        average_list.pop(0)
+    time_left = 0
+    for x in average_list :
+        time_left += x
+    time_left = time_left/len(average_list)
+    time_left = time.strftime("%H:%M:%S",time.gmtime(time_left))
+    print(f"{100*(value):.2f}% eta:{time_left}\r",end="")
+    start = time.time()
 
 print("proccessing")
 
-data_procceser = crochet(analise_event,sel_events,10,loading)
+data_procceser = crochet(analise_event,sel_events,12,loading)
 data_procceser.load_fraction = 10000
 proccessed_data = data_procceser.run()
+print(f"\nDone!")
 
 print("plotting")
 
+start = time.time()
+prev_loading = 0
+current_loading = 0
+average_list = []
+total = len(proccessed_data)
 
-for event in proccessed_data :
+#histagrams
+histagram_electrons = Hist(hist.axis.Regular(200,102,80, label = "Mass (GeV)"))
+histagram_muons = Hist(hist.axis.Regular(200,102,80, label = "Mass (GeV)"))
+
+for index,event in enumerate(proccessed_data) :
     if event == None :
         continue
-    for boson in event :
-        histagram.fill(boson.M())
+    for type,boson in event :
+        if type == 11 :
+            histagram_electrons.fill(boson.M())
+        elif type == 13 :
+            histagram_muons.fill(boson.M())
+    loading(index/total)
 
 print("presenting")
 
-histagram.plot(histtype = "fill")
+histagram_electrons.plot(histtype = "fill")
+plt.show()
+
+histagram_muons.plot(histtype = "fill")
 plt.show()
